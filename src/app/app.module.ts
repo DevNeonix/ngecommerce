16293deccc29bed184b2ff52a8357edf38caaaa1
Jolver André Gil Quevedo <devneonix@gmail.com>
2021-04-11 23:51:28 -0500

    header-socials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import {AppComponent} from './app.component';
 import {CarouselModule} from 'ngx-owl-carousel-o';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { PagesComponent } from './pages/pages.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderSocialsComponent } from './shared/components/header-socials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PagesComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeaderSocialsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/header-socials.component.ts
@@ -0,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header-socials',
+  template: `
+    <div class="header-socials bg-primary">
+      <div class="container">
+        <div class="row py-2 justify-content-between">
+          <div class="col-12 col-md-3 col-lg-2">
+            <div class="social-icons">
+              <ul
+                class="list-unstyled d-flex  justify-content-start justify-content-lg-between align-items-center p-0 m-0"
+              >
+                <li>
+                  <a href="https://www.facebook.com/" class="px-2">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com" class="px-2">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://instagram.com/" class="px-2">
+                    <i class="fab fa-instagram"> </i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/" class="px-2">
+                    <i class="fab fa-youtube"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-12 col-md-3 d-flex justify-content-end">
+            <a href="" class="text-light mx-3">Login</a>
+            <a href="" class="text-light mx-3">Registro</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .social-icons > ul > li > a > i {
+        color: #fff;
+      }
+    `,
+  ],
+})
+export class HeaderSocialsComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
